164674c5220fa9e57ff2bffd3033d41a4c74fb6a
Preston Leigh <preston.leigh32@gmail.com>
2021-12-01 22:24:10 -0600

    Added logic, finished run function and draw function and input functions

--- PONG Game.cpp
+++ PONG Game.cpp
@@ -154,36 +154,212 @@ public:
         player1->Reset();
         player2->Reset();
     }
+    void Draw()
+    {
+        system("cls");
+        for (int i = 0; i < width + 2; ++i)
+        {
+            cout << "\xB2";
+        }
+        cout << endl;
 
+        for (int i = 0; i < height; ++i)
+        {
+            for (int j = 0; j < width; ++j)
+            {
+                int ballx = ball->getX();
+                int bally = ball->getY();
+                int player1x = player1->getX();
+                int player2x = player2->getX();
+                int player1y = player1->getY();
+                int player2y = player2->getY();
+
+                if (j == 0)
+                {
+                    cout << "\xB2";
+                }
+
+                if (ballx == j && bally == i)
+                {
+                    cout << "O"; //ball
+                }
+                else if (player1x == j && player1y == i)
+                {
+                    cout << "\xDB"; // player 1
+                }
+                else if (player2x == j && player2y == i)
+                {
+                    cout << "\xDB"; // player 2
+                }
+
+                else if (player1x == j && player1y + 1 == i)
+                {
+                    cout << "\xDB"; // player 1
+                }
+                else if (player1x == j && player1y + 2 == i)
+                {
+                    cout << "\xDB"; // player 1
+                }
+                else if (player1x == j && player1y + 3 == i)
+                {
+                    cout << "\xDB"; // player 1
+                }
+
+                else if (player2x == j && player2y + 1 == i)
+                {
+                    cout << "\xDB"; // player 1
+                }
+                else if (player2x == j && player2y + 2 == i)
+                {
+                    cout << "\xDB"; // player 1
+                }
+                else if (player2x == j && player2y + 3 == i)
+                {
+                    cout << "\xDB"; // player 1
+                }
+
+                else { cout << " "; }
+
+                if (j == width - 1) { cout << "\xB2"; }
+            }
+            cout << endl;
+        }
+        for (int i = 0; i < width + 2; ++i)
+        {
+            cout << "\xB2";
+        }
+        cout << endl;
+    }
+    void Input()
+    {
+        ball->Move();
+        int ballx = ball->getX();
+        int bally = ball->getY();
+        int player1x = player1->getX();
+        int player2x = player2->getX();
+        int player1y = player1->getY();
+        int player2y = player2->getY();
+
+        if (_kbhit())
+        {
+            char current = _getch();
+            if (current == up1)
+            {
+                if (player1y > 0)
+                {
+                    player1->moveUp();
+                }
+            }
+            if (current == up2)
+            {
+                if (player2y > 0)
+                {
+                    player2->moveUp();
+                }
+            }
+            if (current == down1)
+            {
+                if (player1y + 4 < height)
+                {
+                    player1->moveDown();
+                }
+            }
+            if (current == down2)
+            {
+                if (player2y + 4 < height)
+                {
+                    player2->moveDown();
+                }
+            }
+
+            if (ball->getDirection() == STOP)
+            {
+                ball->randomDirection();
+            }
+            if (current == 'q')
+            {
+                quit = true;
+            }
+        }
+    }
+
+        void Logic()
+        {
+            int ballx = ball->getX();
+            int bally = ball->getY();
+            int player1x = player1->getX();
+            int player2x = player2->getX();
+            int player1y = player1->getY();
+            int player2y = player2->getY();
+
+            //left paddle
+            for (int i = 0; i < 4; ++i)
+            {
+                if (ballx == player1x + 1)
+                {
+                    if (bally == player1y + i)
+                    {
+                        ball->changeDirection((eDir)((rand()%3)+4)); //right, upright, or downright
+                    }
+                }
+            } // end for
+
+            //right paddle
+            for (int i = 0; i < 4; ++i)
+            {
+                if (ballx == player2x - 1)
+                {
+                    if (bally == player2y + i)
+                    {
+                        ball->changeDirection((eDir)((rand() % 3) + 1)); //left, upleft, downleft
+                    }
+                }
+            } // end for
+
+
+            //bottom wall
+            if (bally == height - 1)
+            {
+                ball->changeDirection(ball->getDirection() == DOWNRIGHT ? UPRIGHT : UPLEFT);
+            }
+
+            //topwall
+            if (bally == 0)
+            {
+                ball->changeDirection(ball->getDirection() == UPRIGHT ? DOWNRIGHT : DOWNLEFT);
+            }
+
+            //rightwall
+            if (ballx == width - 1)
+            {
+                ScoreUp(player1);
+            }
+
+            //leftwall
+            if (ballx == 0)
+            {
+                ScoreUp(player2);
+            }
+        }
+
+        void Run()
+        {
+            while (!quit)
+            {
+                Draw();
+                Input();
+                Logic();
+            }
+        }
+    
 };
 
 int main()
 {
-    cBall c(0, 0);
-    cout << c << endl;
-
-    c.randomDirection();
-    cout << c << endl;
-    c.Move();
-    cout << c << endl;
-    c.randomDirection();
-    c.Move();
-    cout << c << endl;
-    c.randomDirection();
-    c.Move();
-    cout << c << endl;
-
-    cPaddle p1(0, 0);
-    p1.moveDown();
-    cout << p1 << endl;
     
+    cGameManager c(40, 20);
 
-    cPaddle p2(10, 0);
-    p2.moveUp();
-    cout << p2 << endl;
-
-
-
+    c.Run();
 
    
     return 0;
